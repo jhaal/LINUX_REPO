84274b9f5adae9664935aa74b58044c7e360d219
jhaalok_deb <jhaalok1984@gmail.com>
2017-07-15 20:41:57 +0530

    Sample CFD problems

--- python/file_rename.py
+++ python/file_rename.py
@@ -2,22 +2,30 @@ import os
 import string
 import itertools
 
-os.chdir('//media/user01/1386EB730F43FD42/Alok_Files/Programming_Books_&_Algorithm_Design')
+os.chdir('/media/user01/1386EB730F43FD42/MIT_6.046J/')
 
 for f in os.listdir():
     #print(f)
-    a = f.strip()
+    #a = f.strip()
     #print(a)
     fname,fext = os.path.splitext(f)
-    #a = fname.split()
-    #print(fname)
-    b = str(fname).ljust(70,'-')
+    #a1,a2,a3,a4,a5,a6,a7,a8,a9 = fname.split('.')
+    a1 =  fname.split('-')
+    #print(a1)
+    a2 = a1[0:2]
+    #print(a2)
+    a3 = ''.join(a2)
+    #print(a3)
+    #print(fext)
+    #b = str(fname).ljust(70,'-')
     #print(b)
-    c = b.strip('-')
+    #c = b.strip('-')
     #print(c)
     #d = c[0:52]
-    #print('{}{}'.format(c,fext))
-    renamed = '{}{}'.format(c,fext)
+    #print('{} {} {} {} {} {} {}.{}'.format(a2.zfill(2),a1,a6,a7,a8,a9,fext))
+    #print('{}{}'.format(a3,fext))
+    renamed = '{}{}'.format(a3,fext)
+    #renamed = '{}{}'.format(c,fext)
     #print(renamed)
     os.rename(f,renamed)
 
